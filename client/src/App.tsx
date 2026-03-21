657280f9cf916f74f978c9d993d724055b109b00
Roh-barboza <rodrigo.barboza1994@gmail.com>
2026-03-21 14:09:34 -0300

    feat: rota /documentos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import TaskGenerator from "./pages/TaskGenerator";
 import CRM from "./pages/CRM";
 import Intranet from "./pages/Intranet";
 import Login from "./pages/Login";
+import Documentos from "./pages/Documentos";
 import Processos from "./pages/Processos";
 
 
@@ -21,6 +22,7 @@ function Router() {
       <Route path="/intranet" component={Intranet} />
       <Route path="/login" component={Login} />
         <Route path="/processos" component={Processos} />
+  <Route path="/documentos" component={Documentos} />
 <Route path="/404" component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
